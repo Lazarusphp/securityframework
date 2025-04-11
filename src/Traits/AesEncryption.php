15ac204dacf16268c2950a5c4d76cf2aef6f20c2
martin bamber <mbamber1986@gmail.com>
2025-04-11 23:16:43 +0100

    Refactored Methods Moved SetEnc and renamed to generateKey

--- src/Traits/AesEncryption.php
+++ src/Traits/AesEncryption.php
@@ -7,18 +7,29 @@ trait AesEncryption
     private static $key = "";
     private static $cipher = 'AES-256-CBC';
 
+    public static function generateKey(?string $key=null)
+    {
+        FileCrafter::bind("SecurityKey",self::$filename,[JsonWriter::class]);
+        FileCrafter::generate("SecurityKey",function($writer)
+        {
+            $key = $key ?? self::setPassword(bin2hex(random_bytes(32)));
+            $date = Date::withAddedTime("now","P3D")->format("Y-m-d H:i:s");
+            $now = Date::create()->format("Y-m-d H:i:s");
+            $writer->preventOverwrite("EncryptionKey","Key","Created");
+            $writer->set("EncryptionKey","Key",$key);
+            $writer->set("EncryptionKey","Created",$now);
+            
+                $writer->save(); 
+            self::setkey($writer->fetch()->EncryptionKey->Key);
+        });
+    }
 
-    public static function setKey($key):void
+    private static function setKey($key):void
     {
          self::$key = $key;
     }
 
-    public static function getKey():string
-    {
-        return self::$key;
-    }   
-
-    public static function encryptValue($value):string
+    public static function encrypt($value):string
     {
         if (!empty(self::$key)) {
             $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(self::$cipher));
@@ -31,14 +42,13 @@ trait AesEncryption
         }
     }
     
-    public static function decryptValue($value):bool|string
+    public static function decscrypt($value):bool|string
     {
         $decoded = base64_decode($value);
         // Extract IV and encrypted data
         $iv_length = openssl_cipher_iv_length(self::$cipher);
         $iv = substr($decoded, 0, $iv_length);
         $encrypted = substr($decoded, $iv_length);
-    
         return openssl_decrypt($encrypted, self::$cipher, self::$key, 0, $iv);
     }
 }
